d8ad8010981cf404e93d93f76b82a525ad0f5700
hasansnetwork <hasansnetwork@gmail.com>
2023-05-22 16:51:46 +0300

    test edildi

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const cors = require('cors');
 const app = express();
 const fs = require('fs');
 const port = 3000;
+const REDIS_PORT=6379;
+const redis = require('redis');
+const redisClient = redis.createClient();
+
+
 
 // load dotenv to read environment variables
 require("dotenv").config();
@@ -21,38 +26,48 @@ const dashboardRouter = require("./routes/dashboard");
 app.use(bodyParser.json());
 app.use(cors()); // CORS'u kullanmak için middleware'ı uygulayın
 
+
 app.post('/api/messages', (req, res) => {
   const message = req.body.message;
-  console.log('Alınan test:', req.body.values[1].id);
-  console.log('Alınan test:', req.body.values[1].v);
-  console.log('Alınan test:', req.body.values[2].id);
-  console.log('Alınan test:', req.body.values[2].v);
-  console.log('Alınan test:', req.body.values[3].id);
-  console.log('Alınan test:', req.body.values[3].v);
-  console.log('Alınan test:', req.body.values[4].id);
-  console.log('Alınan test:', req.body.values[4].v);
+  // console.log('Alınan test:', req.body.values[1].id);
+  // console.log('Alınan test:', req.body.values[1].v);
+  // console.log('Alınan test:', req.body.values[2].id);
+  // console.log('Alınan test:', req.body.values[2].v);
+  // console.log('Alınan test:', req.body.values[3].id);
+  // console.log('Alınan test:', req.body.values[3].v);
+  // console.log('Alınan test:', req.body.values[4].id);
+  // console.log('Alınan test:', req.body.values[4].v);
   //console.log('Alınan mesaj:', message);
+  redisClient.connect();
+  redisClient.on('connect', () => {
+    console.log('Connected to Redis server');
+    //var stringResponse = req.body.message.toString(); 
+    const jsonVerisi = req.body;
+    const metin = JSON.stringify(jsonVerisi);
+    fs.appendFileSync('/Users/hasanaydogmus/Desktop/opcVeri.txt', metin);
+    redisClient.set("Hasan",metin,redis.print);
+    redisClient.get("Hasan",redis.print);
+    //redisClient.disconnect();
+  });
+  
+  // // Redis istemcisi ile bağlantı hatası olduğunda tetiklenecek olay
+  // redisClient.on('error', (err) => {
+  //   console.error('Redis connection error:', err);
+  // });
 
-  // İşlemleriniz burada gerçekleştirilebilir
+
+
 
   // txt yazdırma
-  const jsonVerisi = req.body;
-  const metin = JSON.stringify(jsonVerisi);
-  fs.appendFileSync('/Users/hasanaydogmus/Desktop/opcVeri.txt', metin);
+
 //rep
 
   res.send('POST isteği başarıyla karşılandı');
 
 });
 
-
-
-
-
-
-
-
 app.get("/", dashboardRouter);
+
 
 // app.get("/mqttConnDetails", (req, res) => {
 //   //   res.send(
